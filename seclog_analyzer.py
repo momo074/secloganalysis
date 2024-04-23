6bb801c1e270523e763ccb5a294f76a9e265e94a
momo074 <167190095+momo074@users.noreply.github.com>
2024-04-23 15:11:38 +0200

    Add new function

--- seclog_analyzer.py
+++ seclog_analyzer.py
@@ -1,4 +1,5 @@
 import re
+import math
 
 def main(log_path):
     with open(log_path, 'r') as file:
@@ -8,8 +9,13 @@ def main(log_path):
         if detect_intrusion(log):
             print(f"Suspicious activity detected: {log}")
 
+def intrusion_inc(type):
+    list = []
+    for i in range (0,100):
+        list.append(i)
+        print(list)
+
 def detect_intrusion(log):
-    # Exemple d'expression régulière pour détecter une tentative d'injection SQL
     patterns = [
         r"(UNION SELECT|SELECT.*FROM.*WHERE)",
         r"(DROP TABLE|DROP DATABASE)",
@@ -23,6 +29,6 @@ def detect_intrusion(log):
 if __name__ == "__main__":
     import sys
     if len(sys.argv) < 2:
-        print("Usage: python seclog_analyzer.py /chemin/vers/les/logs")
+        print("Not in the loop : seclog_analyzer.py /chemin/vers/les/logs")
     else:
         main(sys.argv[1])
