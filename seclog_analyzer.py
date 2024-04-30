0859bbe34b8a9e92202a8610e047530e203448c4
momo074 <167190095+momo074@users.noreply.github.com>
2024-04-30 17:22:13 +0200

    Added ipanalyz

--- seclog_analyzer.py
+++ seclog_analyzer.py
@@ -1,6 +1,15 @@
 import re
 import math
 
+import socket
+
+def ip_analyzer(ip_address):
+    try:
+        host_name = socket.gethostbyaddr(ip_address)
+        print(f"L'adresse IP {ip_address} correspond à l'hôte {host_name[0]}")
+    except socket.herror:
+        print(f"Impossible de trouver l'hôte correspondant à l'adresse IP {ip_address}")
+
 def main(log_path):
     with open(log_path, 'r') as file:
         logs = file.readlines()
@@ -12,11 +21,12 @@ def main(log_path):
 def intrusion_inc(type):
     list = []
     for i in range (0,100):
-        list.append(i)
-        print(list)
+        list.append(i) 
 
 
 if __name__ == "__main__":
+    # Test de la fonction
+ip_analyzer("8.8.8.8")
     import sys
     if len(sys.argv) < 2:
         print("Not in the loop : seclog_analyzer.py /chemin/vers/les/logs")
